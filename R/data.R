a172146630616e48a157fa12a774d8f8faaa8d79
Mengyuan Shen <129487421+myushen@users.noreply.github.com>
2024-03-26 14:18:32 +1100

    Revert "import api"

--- R/data.R
+++
@@ -1,8 +0,0 @@
-#' Sample SingleCellExperiment Object
-#'
-#' A sample SCE object from HeOrganAtlasData with transformation in metadata and assay
-#' 
-#' @format An object of class \code{SingleCellExperiment}
-#' @source HeOrganAtlasData Liver 
-#' @keywords datasets
-"sample_sce_obj"
